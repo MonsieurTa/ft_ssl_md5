058548892f0ea2ce0800c3d202a6d0e8476b416c
Ta William <wta@student.42.fr>
2019-10-11 12:42:40 +0200

    chore: add functions prototypes

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -6,13 +6,14 @@
 /*   By: wta <wta@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 17:31:34 by wta               #+#    #+#             */
-/*   Updated: 2019/10/09 16:41:01 by wta              ###   ########.fr       */
+/*   Updated: 2019/10/11 12:37:10 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_SSL_H
 # define FT_SSL_H
 
+#include <stdint.h>
 #include "option.h"
 
 typedef struct	s_env
@@ -28,5 +29,7 @@ typedef struct	s_env
 void    	print_bit(char c, int len);
 uint64_t	align_up(int n, uint64_t mod);
 uint32_t	left_rotate(uint32_t x, uint32_t c);
+void		byte_to_hexa(char *dst, uint8_t byte);
+
 
 #endif
--- includes/md5.h
+++ includes/md5.h
@@ -6,13 +6,14 @@
 /*   By: wta <wta@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 12:54:27 by wta               #+#    #+#             */
-/*   Updated: 2019/10/10 18:53:47 by wta              ###   ########.fr       */
+/*   Updated: 2019/10/11 12:34:42 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MD5_H
 # define MD5_H
 
+# include <stdint.h>
 typedef struct	s_md5
 {
 	uint8_t		*msg;
@@ -22,9 +23,10 @@ typedef struct	s_md5
 	uint32_t	c0;
 	uint32_t	d0;
 	uint32_t	digest[4];
+	char		result[32];
 }				t_md5;
 
-typedef struct	s_md5_hash
+typedef struct	s_md5_digest
 {
 	uint32_t	f;
 	uint32_t	g;
@@ -32,12 +34,21 @@ typedef struct	s_md5_hash
 	uint32_t	b;
 	uint32_t	c;
 	uint32_t	d;
-}				t_md5_hash;
+}				t_md5_digest;
 
-int			md5(t_env *env);
-uint32_t	bitwise_fn_f(uint32_t b, uint32_t c, uint32_t d);
-uint32_t	bitwise_fn_g(uint32_t b, uint32_t c, uint32_t d);
-uint32_t	bitwise_fn_h(uint32_t b, uint32_t c, uint32_t d);
-uint32_t	bitwise_fn_i(uint32_t b, uint32_t c, uint32_t d);
+extern uint32_t	g_left_shifts[];
+extern uint32_t	g_constants[];
+
+void			init_md5_hash(t_md5 *md5, t_md5_digest *md5_hash);
+void			md5_join_result(t_md5 *env_md5);
+void			md5_get_result(t_md5 *env_md5);
+void			assign_round_digest(t_md5_digest *md5_digest, int i, uint32_t *chunk);
+void			assign_digest(t_md5 *env_md5, t_md5_digest *md5_digest);
+
+int				md5(t_env *env);
+uint32_t		bitwise_fn_f(uint32_t b, uint32_t c, uint32_t d);
+uint32_t		bitwise_fn_g(uint32_t b, uint32_t c, uint32_t d);
+uint32_t		bitwise_fn_h(uint32_t b, uint32_t c, uint32_t d);
+uint32_t		bitwise_fn_i(uint32_t b, uint32_t c, uint32_t d);
 
 #endif
